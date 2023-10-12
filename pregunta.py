52899c8a735cc57ae7bf2cf156072b3e7eaea240
Santiago Cano Herrera <scanoh@unal.edu.co>
2023-10-11 19:03:34 -0500

    Revert "Entrega 5"
    This reverts commit b51fac342a016ba5a4bed0ea5fdae892aee2930a.

--- pregunta.py
+++ pregunta.py
@@ -16,18 +16,5 @@ def clean_data():
     #
     # Inserte su código aquí
     #
-    df.drop(['Unnamed: 0'], axis=1,inplace=True)
-    df.dropna(inplace=True)
-
-    col_mayus = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']
-    for columna in col_mayus:
-        df[columna] = df[columna].str.lower()
-
-    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
-    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
-    df["barrio"] = df["barrio"].apply(lambda x: str(x).replace("_"," ").replace("-"," "))
-    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).replace("-", " ").replace("_", " ").strip())
-    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True, errors="coerce")
-    df.drop_duplicates(inplace=True)
 
     return df
